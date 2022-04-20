e443e0dc7bb497bf584483b4728f2da2f3bfe840
Jan Roztočil <janroztocil@avocode.com>
2022-04-20 12:47:16 +0200

    Added fonts/ sources subdir

---
+++ src/fonts/FaceTable.h
@@ -0,0 +1,78 @@
+#pragma once
+
+#include "textify/Face.h"
+
+#include "common/buffer_view.h"
+
+#include <string>
+#include <unordered_map>
+
+namespace textify {
+
+/**
+ * A table containing FreeType faces
+ */
+class FaceTable
+{
+public:
+    struct Item
+    {
+        FacePtr face;
+        std::string storageKey;
+        bool fallback;
+    };
+
+    FaceTable() = default;
+    FaceTable(const FaceTable&) = delete;
+    FaceTable& operator=(const FaceTable&) = delete;
+
+    void initialize(FreetypeHandle& ft);
+
+    struct LoadFaceAsResultRec
+    {
+        /**
+         * Original postScript name as found in the font file.
+         */
+        std::string originalFaceName;
+
+        /**
+         * Key used to look up the face in this table.
+         */
+        std::string faceKey;
+    };
+    using LoadFaceAsResult = Result<LoadFaceAsResultRec, bool>;
+
+    /**
+     * @param   storageKey  key under which the data is stored in  @a textify::FontStorage
+     * @param   faceKey     key to look up the face in this table, if empty, the original
+     *                      postScriptname from the font file is used
+     * @param   faceName    postScript name of the face to load from the buffer,
+     *                      if empty, the first face is loaded
+     *
+     * @return  @a LoadFaceAsResult
+     */
+    LoadFaceAsResult loadFace(const std::string& storageKey, const std::string& faceKey,
+                              const std::string& faceName, BufferView fontData);
+
+    FacesNames loadFaces(const std::string& storageKey, const FacesNames& faces, BufferView fontData);
+    FacesNames loadAllFaces(const std::string& storageKey, BufferView fontData);
+
+    void unloadFacesByStorageKey(const std::string& storageKey);
+
+    void discardFaces();
+    bool exists(const std::string& name) const;
+    const Item* getFaceItem(const std::string& name) const;
+
+    FacesNames listAllFacesNames() const;
+    FacesNames listFacesInStorage(const std::string& storageKey) const;
+
+private:
+    bool loadItem(const std::string& name, Item item);
+
+    using TableType = std::unordered_map<std::string, Item>;
+
+    FreetypeHandle* ft_ = nullptr;
+    TableType faceItems_; ///< The key is a Postscript face name
+};
+
+} // namespace textify
---
+++ src/fonts/FontCodec.h
@@ -0,0 +1,97 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+#include "common/result.hpp"
+#include "textify/base-types.h"
+
+namespace textify {
+namespace codec {
+
+enum Format {
+    CODEC_FORMAT_RAW,
+    CODEC_FORMAT_XOR,
+    CODEC_FORMAT_LZ4,
+    CODEC_FORMAT_MONO,
+    CODEC_FORMAT_LZ4_MONO,
+    CODEC_FORMAT_BAD,
+};
+
+const unsigned int SIGNATURE = 'DCVA';
+
+
+class Codec {
+public:
+    typedef Result<BufferType, bool> Result;
+
+    virtual ~Codec() = default;
+    virtual Result encode(const BufferType & input) = 0;
+    virtual Result decode(const BufferType & input) = 0;
+};
+
+class CodecRaw: public Codec {
+public:
+    Result encode(const BufferType & input) override;
+    Result decode(const BufferType & input) override;
+};
+
+class CodecXOR: public Codec {
+public:
+    Result encode(const BufferType & input) override;
+    Result decode(const BufferType & input) override;
+};
+
+#if defined(TEXTIFY_USE_LZ4)
+class CodecLZ4: public Codec {
+public:
+    Result encode(const BufferType & input) override;
+    Result decode(const BufferType & input) override;
+
+    struct Subheader {
+        unsigned int original_length;
+    };
+};
+#endif
+
+#if defined(TEXTIFY_USE_MONOCYPHER)
+class CodecMonocypher: public Codec {
+public:
+    Result encode(const BufferType & input) override;
+    Result decode(const BufferType & input) override;
+private:
+    const char *key = "secr1234etke1234y__h1234ello1234";
+
+    struct Subheader {
+        uint8_t nonce[ 24 ];
+        uint8_t mac[ 16 ];
+
+        void generate();
+    };
+};
+#endif
+
+#if defined(TEXTIFY_USE_LZ4) && defined(TEXTIFY_USE_MONOCYPHER)
+class CodecLZ4Mono: public Codec {
+private:
+    CodecLZ4 lz4;
+    CodecMonocypher mono;
+public:
+    Result encode(const BufferType & input) override;
+    Result decode(const BufferType & input) override;
+};
+#endif
+
+
+struct Header {
+    unsigned int signature;
+    unsigned int format;
+};
+
+
+
+Codec::Result encode(int format, const BufferType & buffer, bool verify);
+Codec::Result decode(BufferType & buffer);
+
+} // namespace codec
+} // namespace textify
