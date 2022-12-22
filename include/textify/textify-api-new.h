5eec136532b8a0ea8f8feaa3a8a25f91153e50bf
Matúš Ždanský <matus.zdansky@ceros.com>
2022-12-22 12:38:59 +0100

    [Dev] AIP: Add working version of the new textify API.

---
+++ include/textify/textify-api-new.h
@@ -0,0 +1,214 @@
+#pragma once
+
+#include <cstdint>
+#include <functional>
+#include <optional>
+#include <string>
+#include <vector>
+
+#include <map>
+
+#include "textify-api.h"
+
+
+namespace octopus {
+    struct Text;
+}
+
+namespace textify {
+
+struct Context;
+struct TextShape;
+
+/// NEW API:
+/// TODO: Remove all unnecessary API state
+
+// TODO: Matus: Remove and replace
+enum class HorizontalAlign_NEW {
+    DEFAULT,
+    LEFT,
+    RIGHT,
+    CENTER,
+    JUSTIFY,
+    START,
+    END
+};
+// TODO: Matus: Remove and replace
+enum class Decoration_NEW {
+    NONE = 0,
+    UNDERLINE,
+    DOUBLE_UNDERLINE,
+    STRIKE_THROUGH,
+};
+// TODO: Matus: Remove and replace
+struct TypeFeature_NEW {
+    std::string tag;
+    std::int32_t value;
+};
+typedef std::vector<TypeFeature_NEW> TypeFeatures_;
+
+// TODO: Matus: Remove and replace
+/// The values correspond to the values in Octopus
+enum class Ligatures_NEW {
+    NONE = 0,
+    STANDARD = 1,
+    ALTERNATIVE = 2, ///< Inspired by PS behaviour, stands for discretionary and historical
+    ALL = 3
+};
+
+
+// TODO: Matus: Use the full font specifier (family and style?)
+//struct FontSpecifier {
+//    std::string postScriptName;
+//    std::optional<std::string> family;
+//    std::optional<std::string> style;
+//};
+using FontSpecifier = std::string;
+struct Vector2d {
+    double x, y;
+};
+struct PlacedGlyph {
+    /// Glyph codepoint - index within the loaded font file
+    uint32_t glyphCodepoint;
+    /// Glyph position, specified by its four quad corners.
+    struct QuadCorners {
+        Vector2d topLeft, topRight, bottomLeft, bottomRight;
+    } quadCorners;
+    /// Color - TODO: Matus: this is duplicated in ImmediateFormat
+    uint32_t color;
+
+    // TODO: Matus: Temporary data
+    // TODO: Matus: Remove and replace
+    struct Temporary {
+        // Immediate format:
+        // inherited: faceId, size;
+        struct ImmediateFormat {
+            // TODO: Matus: Inherited from GlyphsFormat
+            float size;
+            Ligatures_NEW ligatures;
+            TypeFeatures_ features;
+            std::vector<float> tabStops;
+
+            // TODO: Matus: ImmediateFormat
+            float lineHeight;       // px, always positive
+            float minLineHeight;    // px
+            float maxLineHeight;    // px
+            float letterSpacing;    // px in Sketch, can be negative
+            float paragraphSpacing; // px in Sketch, cannot be negativ
+            float paragraphIndent;  // px x offset of the first line in a paragraph
+            Decoration_NEW decoration;
+            HorizontalAlign_NEW align;
+            bool kerning;
+            bool uppercase;
+            bool lowercase;
+        } format;
+
+        // TODO: Matus: This font face Id should be moved to some other place
+        //   Maybe the glyphs should be groupped by face Ids
+        std::string fontFaceId;
+
+        // TODO: Matus: The codepoint is what I'm intereseted in
+        uint32_t character;     //!< Unicode
+        bool leftToRight;
+        std::optional<bool> lineStart;
+
+        float defaultLineHeight;
+
+        /// Dimensions (scalable)
+        struct Dimensions {
+            float horizontalAdvance;
+            float ascender;     //!< Up to 1px error, see FT reference
+            float descender;    //!< Up to 1px error, see FT reference
+            float bearingX;   //!< X-axis bearing of the glyph
+            float bearingY;   //!< Y-axis bearing of the glyph
+            float lineHeight; // TODO why is this here and also in format?
+        } dimensions;
+    } temp;
+};
+using PlacedGlyphs = std::vector<PlacedGlyph>;
+
+// TODO: Matus: Remove and replace
+struct VisualRun_NEW {
+    long start, end;
+    bool leftToRight;
+    float width;
+};
+
+// TODO: Matus: Remove and replace
+enum class Justifiable_NEW {
+    POSITIVE,
+    NEGATIVE,
+    DOCUMENT
+};
+
+// TODO: Matus: Remove and replace
+struct Line_NEW {
+    long start, end;
+    //! BiDi visual runs.
+    std::vector<VisualRun_NEW> visualRuns;
+    float lineWidth;
+    bool baseDirLeftToRight = true;
+    Justifiable_NEW justifiable;
+};
+using Line_NEW_vec = std::vector<Line_NEW>;
+
+// TODO: Matus: Remove and replace
+struct Paragraph_NEW {
+    PlacedGlyphs glyphs_;
+    Line_NEW_vec lines_;
+};
+using Paragraph_NEW_vec = std::vector<Paragraph_NEW>;
+
+// TODO: Matus: Remove and replace
+struct ShapeTextResult_NEW {
+    Paragraph_NEW_vec paragraphs;
+    Matrix3f textTransform;
+    FRectangle textBoundsNoTransform;
+
+    struct TextParams {
+        enum class VerticalAlign {
+            TOP,
+            CENTER,
+            BOTTOM
+        } verticalAlign;
+
+        enum class BoundsMode {
+            AUTO_WIDTH,    //!< bounds extended horizontally
+            AUTO_HEIGHT,   //!< fixed width, bounds extended vertically
+            FIXED,         //!< bounds of a fixed size
+        } boundsMode;
+
+        float baseline = 0.0f;
+
+        enum class HorizontalPositionPolicy {
+            ALIGN_TO_FRAME,
+            FRAME_AS_HINT,
+        } horizontalPolicy;
+
+        enum class BaselinePolicy {
+            SET,
+            CENTER,
+            OFFSET_ASCENDER,
+            OFFSET_BEARING
+        } baselinePolicy;
+
+        enum class OverflowPolicy {
+            NO_OVERFLOW,
+            CLIP_LINE,
+            EXTEND_LINE,
+            EXTEND_ALL
+        } overflowPolicy;
+    } textParams;
+};
+
+
+/// Shape text.
+ShapeTextResult_NEW shapeText_NEW(ContextHandle ctx,
+                                  const octopus::Text &text);
+
+/// Draw text.
+DrawTextResult drawText_NEW(ContextHandle ctx,
+                            const ShapeTextResult_NEW &textShape_NEW,
+                            void *outputBuffer, int width, int height,
+                            const DrawOptions &drawOptions);
+}
---
+++ src/textify-api-new.cpp
@@ -0,0 +1,330 @@
+
+#include "textify/textify-api-new.h"
+
+#include "textify/textify-api.h"
+
+#include "compat/affine-transform.h"
+#include "compat/basic-types.h"
+
+#include "fonts/FontManager.h"
+
+#include "textify/Config.h"
+#include "textify/Context.h"
+#include "textify/textify.h"
+#include "textify/TextShape.h"
+#include "textify/types.h"
+
+#include "utils/utils.h"
+#include "utils/Log.h"
+#include "utils/fmt.h"
+
+#include "vendor/fmt/core.h"
+
+#include "textify/text-format.h"
+
+#include <octopus/text.h>
+
+#include <cstdint>
+#include <memory>
+#include <optional>
+#include <string>
+#include <vector>
+
+namespace textify {
+namespace {
+compat::Rectangle convertRect(const textify::Rectangle &r) {
+    return compat::Rectangle{r.l, r.t, r.w, r.h};
+}
+FRectangle convertRect(const compat::FRectangle &r) {
+    return FRectangle{r.l, r.t, r.w, r.h};
+}
+compat::FRectangle convertRect(const FRectangle &r) {
+    return compat::FRectangle{r.l, r.t, r.w, r.h};
+}
+Matrix3f convertMatrix(const compat::Matrix3f &m) {
+    return Matrix3f { m.m[0][0], m.m[0][1], m.m[0][2], m.m[1][0], m.m[1][1], m.m[1][2], m.m[2][0], m.m[2][1], m.m[2][2] };
+}
+compat::Matrix3f convertMatrix(const Matrix3f &m) {
+    return compat::Matrix3f { m.m[0][0], m.m[0][1], m.m[0][2], m.m[1][0], m.m[1][1], m.m[1][2], m.m[2][0], m.m[2][1], m.m[2][2] };
+}
+}
+
+TypeFeatures_ convertFromPrivFeatures(const TypeFeatures &features) {
+    TypeFeatures_ features_;
+    for (const TypeFeature &feature : features) {
+        features_.emplace_back(TypeFeature_NEW{ feature.tag, feature.value });
+    }
+    return features_;
+}
+
+TypeFeatures convertFromApiFeatures(const TypeFeatures_ &features_) {
+    TypeFeatures features;
+    for (const TypeFeature_NEW &feature_ : features_) {
+        features.emplace_back(TypeFeature{ feature_.tag, feature_.value });
+    }
+    return features;
+}
+
+PlacedGlyph convertToPlacedGlyph(const priv::GlyphShape &glyph) {
+    const PlacedGlyph::Temporary::Dimensions dimensions {
+        glyph.horizontalAdvance,
+        glyph.ascender,
+        glyph.descender,
+        glyph.bearingX,
+        glyph.bearingY,
+        glyph.lineHeight,
+    };
+
+    PlacedGlyph::Temporary temp;
+
+    temp.format.size = glyph.format.size;
+    temp.format.ligatures = static_cast<Ligatures_NEW>(glyph.format.ligatures);
+    temp.format.features = convertFromPrivFeatures(glyph.format.features);
+    for (float tabStop : glyph.format.tabStops) {
+        temp.format.tabStops.emplace_back(tabStop);
+    }
+
+    temp.format.lineHeight = glyph.format.lineHeight;
+    temp.format.minLineHeight = glyph.format.minLineHeight;
+    temp.format.maxLineHeight = glyph.format.maxLineHeight;
+    temp.format.letterSpacing = glyph.format.letterSpacing;
+    temp.format.paragraphSpacing = glyph.format.paragraphSpacing;
+    temp.format.paragraphIndent = glyph.format.paragraphIndent;
+    temp.format.decoration = static_cast<Decoration_NEW>(glyph.format.decoration);
+    temp.format.align = static_cast<HorizontalAlign_NEW>(glyph.format.align);
+    temp.format.kerning = glyph.format.kerning;
+    temp.format.uppercase = glyph.format.uppercase;
+    temp.format.lowercase = glyph.format.lowercase;
+
+    temp.fontFaceId = glyph.format.faceId;
+    temp.character = glyph.character;
+    temp.leftToRight = glyph.direction == TextDirection::LEFT_TO_RIGHT;
+    temp.lineStart = glyph.lineStart;
+    temp.defaultLineHeight = glyph.defaultLineHeight;
+
+    temp.dimensions.horizontalAdvance = glyph.horizontalAdvance;
+    temp.dimensions.ascender = glyph.ascender;
+    temp.dimensions.descender = glyph.descender;
+    temp.dimensions.bearingX = glyph.bearingX;
+    temp.dimensions.bearingY = glyph.bearingY;
+    temp.dimensions.lineHeight = glyph.lineHeight;
+
+    PlacedGlyph result;
+
+    result.glyphCodepoint = glyph.codepoint;
+    result.quadCorners = {};
+    result.color = glyph.format.color;
+    result.temp = temp;
+
+    return result;
+}
+
+priv::GlyphShape convertToGlyphShape(/*const FontSpecifier &fontSpecifier,*/
+                                     const PlacedGlyph &placedGlyph) {
+    priv::GlyphShape result;
+
+    ImmediateFormat resultFormat;
+
+    resultFormat.faceId = placedGlyph.temp.fontFaceId;
+    resultFormat.size = placedGlyph.temp.format.size;
+    resultFormat.ligatures = static_cast<GlyphFormat::Ligatures>(placedGlyph.temp.format.ligatures);
+    resultFormat.direction = placedGlyph.temp.leftToRight ? TextDirection::LEFT_TO_RIGHT : TextDirection::RIGHT_TO_LEFT;
+    resultFormat.features = convertFromApiFeatures(placedGlyph.temp.format.features);
+    resultFormat.tabStops = placedGlyph.temp.format.tabStops;
+    resultFormat.lineHeight = placedGlyph.temp.format.lineHeight;
+    resultFormat.minLineHeight = placedGlyph.temp.format.minLineHeight;
+    resultFormat.maxLineHeight = placedGlyph.temp.format.maxLineHeight;
+    resultFormat.letterSpacing = placedGlyph.temp.format.letterSpacing;
+    resultFormat.paragraphSpacing = placedGlyph.temp.format.paragraphSpacing;
+    resultFormat.paragraphIndent = placedGlyph.temp.format.paragraphIndent;
+    resultFormat.color = placedGlyph.color;
+    resultFormat.decoration = static_cast<Decoration>(placedGlyph.temp.format.decoration);
+    resultFormat.align = static_cast<HorizontalAlign>(static_cast<HorizontalAlign_NEW>(placedGlyph.temp.format.align));
+    resultFormat.kerning = placedGlyph.temp.format.kerning;
+    resultFormat.uppercase = placedGlyph.temp.format.uppercase;
+    resultFormat.lowercase = placedGlyph.temp.format.lowercase;
+
+    result.format = resultFormat;
+    result.codepoint = placedGlyph.glyphCodepoint;
+    result.character = placedGlyph.temp.character;
+    result.direction = placedGlyph.temp.leftToRight ? TextDirection::LEFT_TO_RIGHT : TextDirection::RIGHT_TO_LEFT;
+    result.lineStart = placedGlyph.temp.lineStart;
+
+    result.defaultLineHeight = placedGlyph.temp.defaultLineHeight;
+    result.horizontalAdvance = placedGlyph.temp.dimensions.horizontalAdvance;
+    result.ascender = placedGlyph.temp.dimensions.ascender;
+    result.descender = placedGlyph.temp.dimensions.descender;
+    result.bearingX = placedGlyph.temp.dimensions.bearingX;
+    result.bearingY = placedGlyph.temp.dimensions.bearingY;
+    result.lineHeight = placedGlyph.temp.dimensions.lineHeight;
+
+    return result;
+}
+
+VisualRun_NEW convertToVisualRun_NEW(const VisualRun &visualRun) {
+    VisualRun_NEW result;
+
+    result.start = visualRun.start;
+    result.end = visualRun.end;
+    result.width = visualRun.width;
+    result.leftToRight = visualRun.dir == TextDirection::LEFT_TO_RIGHT;
+
+    return result;
+}
+
+Line_NEW convertToLineSpan_NEW(const priv::LineSpan &lineSpan) {
+    Line_NEW result;
+
+    result.start = lineSpan.start;
+    result.end = lineSpan.end;
+
+    for (const VisualRun &visualRun : lineSpan.visualRuns) {
+        result.visualRuns.emplace_back(convertToVisualRun_NEW(visualRun));
+    }
+
+    result.lineWidth = lineSpan.lineWidth;
+    result.baseDirLeftToRight = lineSpan.baseDir == TextDirection::LEFT_TO_RIGHT;
+    result.justifiable = static_cast<Justifiable_NEW>(lineSpan.justifiable);
+
+    return result;
+}
+
+VisualRun convertToVisualRun(const VisualRun_NEW &visualRun) {
+    VisualRun result;
+
+    result.start = visualRun.start;
+    result.end = visualRun.end;
+    result.width = visualRun.width;
+    result.dir = visualRun.leftToRight ? TextDirection::LEFT_TO_RIGHT : TextDirection::RIGHT_TO_LEFT;
+
+    return result;
+}
+
+priv::LineSpan convertToLineSpan(const Line_NEW &lineSpan) {
+    priv::LineSpan result;
+
+    result.start = lineSpan.start;
+    result.end = lineSpan.end;
+
+    for (const VisualRun_NEW &visualRun : lineSpan.visualRuns) {
+        result.visualRuns.emplace_back(convertToVisualRun(visualRun));
+    }
+
+    result.lineWidth = lineSpan.lineWidth;
+    result.baseDir = lineSpan. baseDirLeftToRight ? TextDirection::LEFT_TO_RIGHT :  TextDirection::RIGHT_TO_LEFT;
+    result.justifiable = static_cast<priv::LineSpan::Justifiable>(lineSpan.justifiable);
+
+    return result;
+}
+
+
+ShapeTextResult_NEW shapeText_NEW(ContextHandle ctx,
+                                  const octopus::Text &text) {
+    ShapeTextResult_NEW result;
+    if (ctx == nullptr) {
+        return result;
+    }
+
+    priv::TextShapeResult textShapeResult = priv::shapeText(*ctx, text);
+    if (!textShapeResult) {
+        ctx->getLogger().error("shaping of a text failed with error: {}", (int)textShapeResult.error());
+        return result;
+    }
+
+    const priv::TextShapeDataPtr textShapeData = textShapeResult.moveValue();
+    if (textShapeData == nullptr) {
+        return result;
+    }
+
+    result.textTransform = convertMatrix(textShapeData->textTransform);
+    result.textBoundsNoTransform = convertRect(textShapeData->textBoundsNoTransform);
+    result.textParams.baseline = textShapeData->baseline;
+    result.textParams.verticalAlign = static_cast<ShapeTextResult_NEW::TextParams::VerticalAlign>(textShapeData->formattedText->verticalAlign());
+    result.textParams.boundsMode = static_cast<ShapeTextResult_NEW::TextParams::BoundsMode>(textShapeData->formattedText->boundsMode());
+    result.textParams.baselinePolicy = static_cast<ShapeTextResult_NEW::TextParams::BaselinePolicy>(textShapeData->formattedText->baselinePolicy());
+    result.textParams.overflowPolicy = static_cast<ShapeTextResult_NEW::TextParams::OverflowPolicy>(textShapeData->formattedText->overflowPolicy());
+
+    for (const priv::ParagraphShapePtr &paragraphShape : textShapeData->paragraphShapes) {
+        if (paragraphShape == nullptr)
+            continue;
+
+        Paragraph_NEW &pNew = result.paragraphs.emplace_back();
+        for (const priv::GlyphShape &glyph : paragraphShape->glyphs()) {
+            pNew.glyphs_.emplace_back(convertToPlacedGlyph(glyph));
+        }
+        for (const priv::LineSpan &lineSpan : paragraphShape->lineSpans()) {
+            pNew.lines_.emplace_back(convertToLineSpan_NEW(lineSpan));
+        }
+    }
+
+    return result;
+}
+
+DrawTextResult drawText_NEW(ContextHandle ctx,
+                            const ShapeTextResult_NEW &textShape_NEW,
+                            void *outputBuffer, int width, int height,
+                            const DrawOptions &drawOptions)
+{
+    if (ctx == nullptr) {
+        return {};
+    }
+
+    priv::ParagraphShapes paragraphShapes;
+
+    for (const Paragraph_NEW &paragraphShape : textShape_NEW.paragraphs) {
+        priv::ParagraphShapePtr &paragraph = paragraphShapes.emplace_back(std::make_unique<priv::ParagraphShape>(ctx->getLogger(), ctx->getFontManager().facesTable()));
+
+        priv::GlyphShapes glyphs;
+        priv::LineSpans lineSpans;
+
+        for (const PlacedGlyph &placedGlyph : paragraphShape.glyphs_) {
+            glyphs.emplace_back(convertToGlyphShape(placedGlyph));
+        }
+        for (const Line_NEW &lineNew : paragraphShape.lines_) {
+            lineSpans.emplace_back(convertToLineSpan(lineNew));
+        }
+
+        paragraph->initialize(glyphs, lineSpans);
+    }
+
+//    if (textShape && sanitizeShape(ctx, textShape)) {
+
+    const compat::Rectangle viewArea = drawOptions.viewArea.has_value()
+        ? convertRect(drawOptions.viewArea.value())
+        : compat::INFINITE_BOUNDS;
+
+    const priv::FormattedText::FormattingParams formattedTextParams {
+        static_cast<VerticalAlign>(textShape_NEW.textParams.verticalAlign),
+        static_cast<BoundsMode>(textShape_NEW.textParams.boundsMode),
+        textShape_NEW.textParams.baseline,
+        static_cast<HorizontalPositionPolicy>(textShape_NEW.textParams.horizontalPolicy), // TODO: Matus: This does not get set and is unused. Have a closer look!
+        static_cast<BaselinePolicy>(textShape_NEW.textParams.baselinePolicy),
+        static_cast<OverflowPolicy>(textShape_NEW.textParams.overflowPolicy),
+    };
+
+    const compat::Matrix3f textTransform = convertMatrix(textShape_NEW.textTransform);
+    const compat::FRectangle textBoundsNoTransform = convertRect(textShape_NEW.textBoundsNoTransform);
+    const priv::TextDrawResult result = priv::drawText(*ctx,
+                                                       paragraphShapes,
+                                                       textTransform,
+                                                       textBoundsNoTransform,
+                                                       formattedTextParams,
+                                                       textShape_NEW.textParams.baseline,
+                                                       outputBuffer, width, height,
+                                                       drawOptions.scale,
+                                                       false,
+                                                       viewArea);
+    if (result) {
+        const auto& drawOutput = result.value();
+        return {
+            utils::castRectangle(drawOutput.drawBounds), utils::castMatrix(drawOutput.transform),
+            false
+        };
+    }
+
+//    }
+
+    return DrawTextResult {};
+}
+
+} // namespace textify
