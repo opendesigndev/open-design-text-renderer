25339a74356f73b86af8240e8bd802ff97d8f72a
Matúš Ždanský <matus.zdansky@ceros.com>
2023-01-18 10:34:40 +0100

    [Dev] Fix a bug in new Textify when font size is not a whole number (e.g. subscript).

--- src/textify/textify.cpp
+++ src/textify/textify.cpp
@@ -657,7 +657,7 @@ GlyphPtr renderPlacedGlyph(const PlacedGlyph_pr &placedGlyph,
                            bool internalDisableHinting) {
     // TODO: Matus: Here I need `format.size` and `ascender`
     float glyphScale = 1.0f;
-    const font_size desiredSize = face->isScalable() ? std::ceil(placedGlyph.temp.size * scale) : placedGlyph.temp.size;
+    const font_size desiredSize = face->isScalable() ? (placedGlyph.temp.size * scale) : placedGlyph.temp.size;
     const Result<font_size,bool> setSizeRes = face->setSize(desiredSize);
     if (setSizeRes && !face->isScalable()) {
         glyphScale = (placedGlyph.temp.ascender * scale) / (float)setSizeRes.value();
