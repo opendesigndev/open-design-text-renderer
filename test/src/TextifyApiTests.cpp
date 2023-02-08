818dfc086bbc8ecef9bd28c4d8138998cda03e73
Matúš Ždanský <matus.zdansky@ceros.com>
2023-02-08 16:22:21 +0100

    [Test] Add an empty test project for Textify.

---
+++ test/src/TextifyApiTests.cpp
@@ -0,0 +1,13 @@
+
+#include <memory>
+#include <gtest/gtest.h>
+
+
+//! TextifyApiTests class tests.
+class TextifyApiTests : public ::testing::Test {
+};
+
+
+TEST_F(TextifyApiTests, always) {
+    ASSERT_TRUE(true);
+}
