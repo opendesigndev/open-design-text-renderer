489ff84a997a2fd28aa7baf5e632164a18851bac
Matúš Ždanský <matus.zdansky@ceros.com>
2023-02-28 09:59:19 +0100

    [Bug] Fix rename directory to lowercase.

---
+++ src/api/PlacedTextData.cpp
@@ -0,0 +1,18 @@
+
+#include <open-design-text-renderer/PlacedTextData.h>
+
+namespace odtr {
+
+PlacedTextData::PlacedTextData(PlacedGlyphsPerFont &&glyphs_,
+                               PlacedDecorations &&decorations_,
+                               const FRectangle &textBounds_,
+                               const Matrix3f &textTransform_,
+                               float baseline_) :
+    glyphs(std::move(glyphs_)),
+    decorations(std::move(decorations_)),
+    textBounds(textBounds_),
+    textTransform(textTransform_),
+    baseline(baseline_) {
+}
+
+} // namespace odtr
